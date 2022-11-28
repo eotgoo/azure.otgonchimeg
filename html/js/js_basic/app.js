ca6bc85de3555ca842fff45d8bae85e1f877f19f
Evening <evening@mstars-lab3-10s-iMac.local>
2022-11-28 17:33:51 +0800

    pinecone dr 11/28

---
+++ html/js/js_basic/app.js
@@ -0,0 +1,57 @@
+console.log( "hello-world");
+
+let ner = "otgoo"; color = "black";
+let age = 18;
+
+console.log(ner);
+console.log(age);
+console.log(color);
+
+console.log(typeof ner);
+console.log(typeof age);
+console.log(typeof color);
+
+   // task 1 2 3
+//"Comments can make code readable"
+//"Javascript lesson begin"
+/* "Comments can make code readable" */
+
+//variable.js
+
+let uurinner = "naraa";
+
+console.log(uurinner)
+
+/*
+   let firstname = "otgoo";
+   let lastname = "erdene";
+   let country = "mgl";
+
+   connsole.log(firstname);
+   console.log(lastname);
+   console.log(country); 
+*/
+
+ let myage = 18;
+ let yourage = 21;
+
+ console.log(myage)
+ console.log(yourage)
+//triangleS
+ let a = 10;
+ let b = 5;
+
+ console.log("a+b/2=" , a+b/2);
+
+//wheelS
+ let p = 3.14;
+ let r = 5;
+
+ console.log("s=p*r2" , p+r**2 )
+
+//task13
+
+ let y = 1;
+ let x= 1;
+
+ console.log("m = y2-y1/x2-x1" , y**2-y**1/x**2-x**1)
