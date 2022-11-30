8e5dc72f767bb7a744bfc6b2334b0ebf18824f12
Evening <evening@mstars-lab3-10s-iMac.local>
2022-11-30 18:16:58 +0800

    pinecone dr 11/30

---
+++ html/js/js_basic/day3.js
@@ -0,0 +1,46 @@
+//task1 string
+console.log('Hello I am "Otgoo". \nI am "18" years old. \n This is the new line with tab and some \backslashes\.')
+
+
+//task2-1 string
+let ner = 'otgonchimeg';
+let nernitoo = ner.length;
+
+let ovog = 'erdenechimeg';
+let ovogniner = ovog.length;
+
+if(nernitoo>=ovogniner) {
+    console.log('nernitoo urt')
+} else {
+    console.log('ovogniner urt')
+}
+
+
+//task2-2 string
+let text = 'HELLO WORLD';
+console.log(text.toLowerCase());
+
+
+//task2-3 string
+let text1 = 'hello world';
+console.log(text1.toUpperCase());
+
+
+//task2-4 string
+let a = 'Pinecone academy';
+let b = a.concat('' ,' ' ,'leap хөтөлбөрт тавтай морилго уу?');
+console.log(b);
+
+
+//task2-5 string
+let firstname = 'Erdene';
+let lastname = 'Otgoo';
+let country = 'Mongolia';
+let city = 'UB';
+
+console.log('minii neriig' + ' ' + firstname + ' ' + 'ovogtoi' + ' ' + lastname + ' ' + 'gedeg.' + ' ' + 'bi' + ' ' + country + ' ' + 'ulsiin' + ' ' + city + ' ' + 'hotod amidardag.bye bye bye!!');
+
+
+//task2-6 string
+let number = 12345;
+
