d844f8cf5c654c7a6ed63f92b96a8d002126d3fc
eotgoo <ekotgoo@gmail.com>
2022-12-02 00:11:04 +0800

    gerte Ugtah bagshin taskud yg unende amarhan bish T.T

--- html/js/day4/day4.js
+++ html/js/day4/day4.js
@@ -1,8 +1,45 @@
 //task1-1
+
+
 let count = 1;
 while (count <= 10) {
     console.log(count);
-    count += 1;
+    count = count + 1;
+}
+//task1-2
+
+
+let oddNumber = 1;
+while (oddNumber <= 10) {
+    console.log(oddNumber);
+    oddNumber = oddNumber + 2;
+}
+//task1-3
+
+
+let evenNumber = 2;
+while (evenNumber <= 10) {
+    console.log(evenNumber);
+    evenNumber = evenNumber +2;
 }
 
-//task 1-100natural
+
+//task1-4   
+let sum = 1;
+let a = 2;
+do {
+    console.log(sum);
+    sum += a;
+    a++;
+} while(a <= 101);
+
+
+//task1-5
+let n = prompt('ta 20s baga too oruulna uu?')
+do {
+    console.log(n);
+    n++;
+} while(n<=20)
+
+
+//task1-6
