5c199528e7fde8e2414813fa4341b68e2e191486
Evening <evening@mstars-lab3-10s-iMac.local>
2022-12-02 17:36:06 +0800

    pinecone dr 12/02

--- html/js/day4/day4.js
+++ html/js/day4/day4.js
@@ -1,45 +1,113 @@
-//task1-1
+//task1-1  "1-10 hurtelh natural too"
+
+console.log ('1-10 hurtelh natural too');
+
+ let count = 1;
+ while (count <= 10) {
+     console.log(count);
+     count = count + 1;
+ }
 
 
-let count = 1;
-while (count <= 10) {
-    console.log(count);
-    count = count + 1;
-}
-//task1-2
+
+//task1-2  "1-10 hurtelh natural sondgoi too"
+
+console.log ('1-10 hurtelh natural sondgoit too');
+
+ let oddNumber = 1; 
+ while (oddNumber <= 10) {     
+    console.log(oddNumber);     
+    oddNumber = oddNumber + 2; }
 
 
-let oddNumber = 1;
-while (oddNumber <= 10) {
-    console.log(oddNumber);
-    oddNumber = oddNumber + 2;
-}
-//task1-3
 
+//task1-3  "1-10 hurtelh natural tegsh too 
 
-let evenNumber = 2;
-while (evenNumber <= 10) {
+console.log ('1-10 hurtelh natural tesgh too');
+
+ let evenNumber = 2;
+ while (evenNumber <= 10) {     
     console.log(evenNumber);
-    evenNumber = evenNumber +2;
-}
+    evenNumber = evenNumber +2; }
 
 
-//task1-4   
-let sum = 1;
-let a = 2;
-do {
-    console.log(sum);
-    sum += a;
-    a++;
+
+ //task1-4   "1-100 hurtelh natural tooni niilber"
+
+console.log ('1-100 hurtelh natural tooni niilber');
+
+ let sum = 1;
+ let a = 2; do {    
+     console.log(sum);    
+     sum += a;    
+     a++; 
 } while(a <= 101);
 
 
-//task1-5
-let n = prompt('ta 20s baga too oruulna uu?')
-do {
-    console.log(n);
-    n++;
-} while(n<=20)
+
+//task1-5   "n hurtelh toog hewleh niilber oloh"
+
+console.log ('n hurtelh toonii niilber');
+
+ let n = prompt('ta duriin toogo oruulna uu?');
+ let c = 1;
+ let s = 0;
+  do {
+    console.log(c);
+    s = s + c;
+    c = c + 1;
+} while(c<=n)
+console.log('niilber ni ;' , s);
 
 
-//task1-6
+
+//task1-6 "anhnii too mun esehig shalgah"
+
+let firstNumber = prompt('ta toogoo oruulna uu');
+let too = Number(firstNumber);
+
+let i = 2;
+let anhniiToo = true;
+
+if(too === 2){
+    console.log("Anhnii too mun");
+}else{
+    while(i<too){
+        if(too % i === 0){
+            anhniiToo = false;
+            break;
+        }
+        i++;
+    }
+
+    if(anhniiToo){
+        console.log("Anhnii too mun");
+    }else{
+        console.log("Anhnii too bish");
+    }
+}
+
+
+
+//task1-7 "ugugdsun interval dahi anhni toog hewleh"
+
+let lowerNumber = 1;
+let higherNumber = 10;
+let l=2 ;
+let m;
+
+if (m = lowerNumber) {
+} else if (m <= higherNumber) {
+
+}
+
+
+
+//task1-8 "n tooni paktorial oloh"
+
+
+
+
+
+
+
---
+++ html/js/day5/day5.js
@@ -0,0 +1,7 @@
+//task1  "ugugdsun tooni niilberig ol"
+let a = prompt('ta too oruulna uu');
+let sum = number(a);
+while(sum>0) {
+    console.log(sum);
+    sum = sum +1;
+}
