4ff33c0603bcaed367119ae070b3854de141b659
eotgoo <ekotgoo@gmail.com>
2022-12-05 21:49:15 +0800

    nemle

--- html/js/js-basic/basic.js
+++ html/js/js-basic/basic.js
@@ -8,111 +8,117 @@
 
    // task 6 7 "utga onooson, onoogoogui huwisagch zarlah"
 
- let uurinner = "naraa";
- let car;
+//  let uurinner = "naraa";
+//  let car;
 
- console.log(uurinner);
- console.log(car);
+//  console.log(uurinner);
+//  console.log(car);
 
 
    // task 8 9  "olon bolon neg murund huwisagchid zarlah"
 
- let firstname = "otgoo";
- let lastname = "erdene";
- let maritalstatus = "single";
- let country = "mgl";
+//  let firstname = "otgoo";
+//  let lastname = "erdene";
+//  let maritalstatus = "single";
+//  let country = "mgl";
 
- let haha ="" ; let hoho =""; let hihi = "" ; let huhu = "";
+//  let haha ="" ; let hoho =""; let hihi = "" ; let huhu = "";
 
 
    //task10  "gurwaljingin primetr oloh"
    
- let a = 10;
- let b = 5;
- let c = 15;
+//  let a = 10;
+//  let b = 5;
+//  let c = 15;
 
- console.log("a+b+c=" , a+b+c);
+//  console.log("a+b+c=" , a+b+c);
 
 
    //task11   "toirgiin S oloh"
 
- let pi = 3.14;
- let r = 5;
+//  let pi = 3.14;
+//  let r = 5;
 
- console.log("pi*r2=" , pi+r**2 );
+//  console.log("pi*r2=" , pi+r**2 );
 
 
    //task12  "m = y2-y1/x2-x1 хариуг олох"
 
- let y = 1;
- let x= 1;
+//  let y = 1;
+//  let x= 1;
 
- console.log("m = y2-y1/x2-x1" , y**2-y**1/x**2-x**1);
+//  console.log("m = y2-y1/x2-x1" , y**2-y**1/x**2-x**1);
 
    //task13   "15gb ni heden bittei tenstu ve"
 
- let gb = 15;
- console.log("15gb=" , gb*2**30*8);
+//  let gb = 15;
+//  console.log("15gb=" , gb*2**30*8);
 
 
    //task14  "15 МB data 3s BIT Rate ol"
 
- let mb = 15;
+//  let mb = 15;
 
- console.log("15mb=" , mb*1024);
- console.log("Bps=" , mb*1024*3);
+//  console.log("15mb=" , mb*1024);
+//  console.log("Bps=" , mb*1024*3);
 
 
    //task15  "side 1 2 3 gesen huwisagch zarlan S ol"
 
- let side1 = 10;
- let side2 = 20;
- let side3 = 10;
+//  let side1 = 10;
+//  let side2 = 20;
+//  let side3 = 10;
 
- console.log("triagnleS=" , 1/2*side1*side2);
+//  console.log("triagnleS=" , 1/2*side1*side2);
 
 
    //task16   "Өгсөн тоог цельсээс фаренгейт рүү хөрвүүл"
    //task17   "Өгсөн тоог фаренгейтээс цельс рүү хөрвүүл."
 
- let tse = 34;
- let f = 56;
+//  let tse = 34;
+//  let f = 56;
 
- console.log("F=" , 1.8*34+32);
- console.log("C=" , (f-32)/1.8);
+//  console.log("F=" , 1.8*34+32);
+//  console.log("C=" , (f-32)/1.8);
 
 
    //task18 "2 huwisagch zarlan 2 toon utga ugch mathin 5uildel hii"
 
- let l = 20;
- let m = 10;
+//  let l = 20;
+//  let m = 10;
 
- console.log("2 toonii niilber:" , l+m);
- console.log("2 toonii ylgawar:" , l-m);
- console.log("2 toonii nogdwor:" , l/m);
- console.log("2 toonii urjwer:" , l*m);
- console.log("2 toon zeregt dewshulwel:" , l**2 , m**2)
+//  console.log("2 toonii niilber:" , l+m);
+//  console.log("2 toonii ylgawar:" , l-m);
+//  console.log("2 toonii nogdwor:" , l/m);
+//  console.log("2 toonii urjwer:" , l*m);
+//  console.log("2 toon zeregt dewshulwel:" , l**2 , m**2)
 
 
    //task19   "cm -г inch рүү хөрвүүлэх. inch -г cm рүү хөрвүүлэх"
 
- let cm = 50;
- let inch = 35;
+//  let cm = 50;
+//  let inch = 35;
 
- console.log("cm=" , cm/2.54);
- console.log("inch=" , inch*2.54);
+//  console.log("cm=" , cm/2.54);
+//  console.log("inch=" , inch*2.54);
 
 
    //task20   "garaas r oruulan duguin V bolon P ol "
 
- let radius = 3;
- let pii = 3.14;
+//  let radius = 3;
+//  let pii = 3.14;
  
- console.log("duguin ezelhuun:" , 4/3*pii*radius**3);
- console.log("duguin hureni urt:" , 2*pii*radius);
+//  console.log("duguin ezelhuun:" , 4/3*pii*radius**3);
+//  console.log("duguin hureni urt:" , 2*pii*radius);
 
 
    //task21    "3 оронтоо тооны хамгийн эхний цифрийг олох илэрхийлэл бич"
+
+//  let too = 735;
+
+//  console.log( too = too /100,  Math.round(too))
+ 
+
 
    //task22   "Baby Shark"
 
