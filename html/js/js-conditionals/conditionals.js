4f428f54606c1088fc9ed148d2b80af69944cae4
eotgoo <ekotgoo@gmail.com>
2022-12-05 10:33:35 +0800

    za

--- html/js/js-conditionals/conditionals.js
+++ html/js/js-conditionals/conditionals.js
@@ -1,222 +1,317 @@
    //task1   "Өгөгдсөн 4 тооны 80-аас их тоонуудын нийлбэрийг ол"
 
- let a = 85;
- let b = 75;
- let c = 96;
- let d = 69;
+//  let a = 85;
+//  let b = 75;
+//  let c = 96;
+//  let d = 69;
 
- let sum = 0 ;
- if ( a > 80 ){
-   sum = sum+a;
- }
- if ( b > 80 ){
-    sum=sum+b;
- }
- if ( c > 80 ){
-    sum=sum+c;
- }
- if ( d > 80 ){
-    sum=sum+d;
- }
- console.log("sum=" , sum);
+//  let sum = 0 ;
+//  if ( a > 80 ){
+//    sum = sum+a;
+//  }
+//  if ( b > 80 ){
+//     sum=sum+b;
+//  }
+//  if ( c > 80 ){
+//     sum=sum+c;
+//  }
+//  if ( d > 80 ){
+//     sum=sum+d;
+//  }
+//  console.log("sum=" , sum);
    
 
     //task1-2   "Өгөгдсөн 4 тооны 5-аас бага тоонуудын үржвэрийг ол"
 
- let number1 = 2;
- let number2 = 3;
- let number3 = 6;
- let number4 = 5;
- let number = 1;
+//  let number1 = 2;
+//  let number2 = 3;
+//  let number3 = 6;
+//  let number4 = 5;
+//  let number = 1;
 
- if (number1 >= 5) {
-    number=number1*number;
- }
- if (number2 >= 5) {
-    number=number2*number;
- }
- if (number3 >= 5) {
-    number=number3*number;
- }
- if (number4 >= 5) {
-    number=number4*number;
- }
- console.log("urjwer=" , number);
+//  if (number1 >= 5) {
+//     number=number1*number;
+//  }
+//  if (number2 >= 5) {
+//     number=number2*number;
+//  }
+//  if (number3 >= 5) {
+//     number=number3*number;
+//  }
+//  if (number4 >= 5) {
+//     number=number4*number;
+//  }
+//  console.log("urjwer=" , number);
 
 
     //task2   "Эхлээд console.log() ашиглахгүйгээр дараах харьцуулалтын илэрхийллийн үр дүнг тодорхойл."
 
- let i = 4 > 3; //true
- let iii = 4 < 3; //false
- let ii = 4 >= 3; //true
- let iv = 4 <= 3; //false
- let v = 4 == 4; //true
- let vi = 4 === 4; //true
- let vii = 4 != 4; //false
- let iix = 4 !== 4; //false
- let ix = 4 != "4"; //false
- let x = 4 == "4"; //true
- let xi = 4 === "4"; //false
+//  let i = 4 > 3; //true
+//  let iii = 4 < 3; //false
+//  let ii = 4 >= 3; //true
+//  let iv = 4 <= 3; //false
+//  let v = 4 == 4; //true
+//  let vi = 4 === 4; //true
+//  let vii = 4 != 4; //false
+//  let iix = 4 !== 4; //false
+//  let ix = 4 != "4"; //false
+//  let x = 4 == "4"; //true
+//  let xi = 4 === "4"; //false
 
- console.log(i , iii, ii, iv, v, vi, vii, iix, ix, x, xi);
+//  console.log(i , iii, ii, iv, v, vi, vii, iix, ix, x, xi);
 
 
     //task2-2    "Эхлээд console.log() ашиглахгүйгээр дараах харьцуулалтын илэрхийллийн үр дүнг тодорхойл."
 
- let one = 4 > 3 && 10 < 12; //true
- let two = 4 > 3 && 10 > 12; //false
- let three = 4 > 3 || 10 < 12; //true
- let four = 4 > 3 || 10 > 12; //true
- let five = !(4 > 3); //false
- let six = !(4 < 3); //true
- let eight = !(4 > 3 && 10 < 12); //false
- let nine = !(4 > 3 && 10 > 12); //true
- let ten = !(4 === "4"); //true
+//  let one = 4 > 3 && 10 < 12; //true
+//  let two = 4 > 3 && 10 > 12; //false
+//  let three = 4 > 3 || 10 < 12; //true
+//  let four = 4 > 3 || 10 > 12; //true
+//  let five = !(4 > 3); //false
+//  let six = !(4 < 3); //true
+//  let eight = !(4 > 3 && 10 < 12); //false
+//  let nine = !(4 > 3 && 10 > 12); //true
+//  let ten = !(4 === "4"); //true
 
- console.log (one, two, three, four, five, six, eight, nine, ten);
+//  console.log (one, two, three, four, five, six, eight, nine, ten);
 
 
     //task2-3   ".if … else ашиглан myAge болон yourAge-ийн утгыг харьцуул.prompt
 
- let myAge = 18;
- let yourAge =  prompt("та насаа оруулна уу");
+//  let myAge = 18;
+//  let yourAge =  prompt("та насаа оруулна уу");
 
- if ( yourAge > myAge) {
-    console.log('хөгшин байна');
- }else if (yourAge < myAge ){
-    console.log('залуухан байна');
- }
+//  if ( yourAge > myAge) {
+//     console.log('хөгшин байна');
+//  }else if (yourAge < myAge ){
+//     console.log('залуухан байна');
+//  }
 
 
     //task2-4   "Амралтын өдөр эсвэл ажлын өдөр эсэхийг шалгаарай. "
 
- let days = prompt('та өдрөө оруулна уу');
- let daysname;
+//  let days = prompt('та өдрөө оруулна уу');
+//  let daysname;
 
- if (days == 1) {
-    daysname = 'ажлын өдөр';
- } else if (days == 2) {
-    daysname = 'ажлын өдөр';
- } else if (days == 3) {
-    daysname = 'ажлын өдөр';
- } else if (days == 4) {
-    daysname = 'ажлын өдөр';
- } else if (days == 5) {
-    daysname = 'ажлын өдөр';
- } else if (days == 6) {
-    daysname = 'амралтын өдөр';
- } else if (days == 7) {
-    daysname = 'амралтын өдөр';
- } else (daysname = 'Буруу тоо')
+//  if (days == 1) {
+//     daysname = 'ажлын өдөр';
+//  } else if (days == 2) {
+//     daysname = 'ажлын өдөр';
+//  } else if (days == 3) {
+//     daysname = 'ажлын өдөр';
+//  } else if (days == 4) {
+//     daysname = 'ажлын өдөр';
+//  } else if (days == 5) {
+//     daysname = 'ажлын өдөр';
+//  } else if (days == 6) {
+//     daysname = 'амралтын өдөр';
+//  } else if (days == 7) {
+//     daysname = 'амралтын өдөр';
+//  } else (daysname = 'Буруу тоо')
 
- console.log(daysname);
+//  console.log(daysname);
 
 
     //task2-5   "Хэрэглэгчээс цаг оруулах, цаг тутамд үнэлгээ өгөх скрипт бичээрэй."
 
- let hours = prompt('та цагаа оруулна уу');
- let ratePerHour = 28;
+//  let hours = 40;
+//  let ratePerHour = 28;
 
- console.log("Таны ажилласан цагийн хөлс:" , hours*28);
+//  console.log("Таны ажилласан цагийн хөлс:" , hours*28);
 
 
     //task3    "Undur jil oloh"
 
- let year = prompt('та жилээ оруулна уу');
+//  let year = prompt('та жилээ оруулна уу');
 
- if ( year%400===0 ) {
-    console.log('mon');
- } else if ( year%100===0 ) {
-    console.log('bish');
- } else if ( year%400===0 ) {
-    console.log('mon');
- } else {
-    console.log('bish');
- }
+//  if ( year%400===0 ) {
+//     console.log('undur jil mon');
+//  } else if ( year%100===0 ) {
+//     console.log('undur jil bish');
+//  } else if ( year%400===0 ) {
+//     console.log('undur jil mon');
+//  } else {
+//     console.log('undur jil bish');
+//  }
 
 
     //task4   " 3huwisagch zarlan hamgin ih bolon baga toog console deer hewlene uu" 
 
- let e = 1;
- let f = 2;
- let g = 3;
+//  let e = 1;
+//  let f = 2;
+//  let g = 3;
 
- console.log('hamgiin ih too:',g);
- console.log('hamgiin baga too:',e);
+//  console.log("hamgin ih too:" , e);
+//  console.log("hamgin baga too:" , e)
 
+    //task5  "4 toonii baga console dr hewlene uu"
 
-    //task5  "4 toonii baga"
+//  let h = 120;
+//  let l = 33;
+//  let m = 10;
+//  let s = 12;
 
- let h = 120;
- let l = 33;
- let m = 10;
- let n = 12;
+//  console.log('baga too:' , m)
+ 
 
+    //task6  "suragchdin dung shalgarai"
 
+//  let name1 = 'Bat';
+//  let name1Dun = 69;
 
+//  if(name1Dun<=59) {
+//     console.log('mash muu');
+//  } else if(name1Dun>=69) {
+//     console.log('hangalttai')
+//  } else if(name1Dun>=79) {
+//     console.log('dund');
+//  } else if(name1Dun>=89) {
+//     console.log('sain');
+//  } else if(name1Dun>=100) {
+//     console.log('mash sain');
+//  }
 
-//task6
-let name1 = 'Bat';
-let name1Dun = 69;
+//  let name2 = "onts dumd";
+//  let name2Dun = 56;
 
-if(name1Dun<=59) {
-    console.log('mash muu');
-} else if(name1Dun>=69) {
-    console.log('hangalttai')
-} else if(name1Dun>=79) {
-    console.log('dund');
-} else if(name1Dun>=89) {
-    console.log('sain');
-} else if(name1Dun>=100) {
-    console.log('mash sain');
-}
+//  if(name2Dun<=59) {
+//     console.log('mash muu');
+//  } else if(name2Dun>=69) {
+//     console.log('hangalttai');
+//  } else if(name2Dun>=79) {
+//     console.log('dund');
+//  } else if(name2Dun>=89) {
+//     console.log('sain');
+//  } else if(name2Dun>=100) {
+//     console.log('mash sain');
+//  }
 
-let = 'name2';
-let name2Dun = 56;
+    //task7   "Тоогоор илэрхийлсэн дүн буюу n-д утга онооход түүнд харгалзах үсгийг / A,B,C,D,F / хэвлэнэ"
 
-if(name2Dun<=59) {
-    console.log('mash muu');
-} else if(name2Dun>=69) {
-    console.log('hangalttai');
-} else if(name2Dun>=79) {
-    console.log('dund');
-} else if(name2Dun>=89) {
-    console.log('sain');
-} else if(name2Dun>=100) {
-    console.log('mash sain');
-}
+//  let dun=75;
 
-
-//task7
-// let weekNumber = 1;
-// let weekName;
-// if (weekNumber == 1) {
-// weekName = 'Даваа';
-// } else if (weekNumber == 2) {
-// weekName = 'Мягмар';
-// } else if (weekNumber == 3) {
-// weekName = 'Лхагва';
-// } else if (weekNumber == 4) {
-// weekName = 'Пүрэв';
-// } else if (weekNumber == 5) {
-// weekName = 'Баасан';
-// } else if (weekNumber == 6) {
-// weekName = 'Бямба'
-// } else if (weekNumber == 7) {
-// weekName = "Ням"
-// } else {
-// weekName = "Буруу тоо"
-// }
-// console.log(weekName);
+//  if (dun >= 90) {
+//     console.log("A");
+//  } else if (dun >= 80) {
+//     console.log("B");
+//  } else if (dun >= 70) {
+//     console.log("C");
+//  } else if (dun >= 60) {
+//     console.log("D") ;
+//  } else if ( dun <= 50) {
+//     console.group("F")
+//  }
 
 
-// let gender = 'male';
-// let age = 19;
+    //task7-2  "sariin udriin toog hewleh"
 
-// if (gender === 'male' && age >= 18) {
-//     console.log('насанд хүрсэн эрэгтэй');
-// }else {
-//     console.log('насанд хүрээгүй эсвэл хүйс тохирохгүй байна');
-// }
+//  let n = 2;
 
+//  if ( n == 1) {
+//     console.log('1 degur sar 30 or 31');
+//  } else if ( n == 2 ) {
+//     console.log('2 dugar sar 28 or 29');
+//  } else if ( n == 3 ) {
+//     console.log('3 dugar sar 29 or 30 or 31');
+//  } else if ( n == 4 ) {
+//     console.log('4 duger sar 30 or 31');
+//  } else if ( n == 5 ) {
+//     console.log('5 dugar sar 30 or 31');
+//  } else if ( n == 6 ) {
+//     console.log('6 dugar sar 30 or 31');
+//  } else if ( n == 7 ) {
+//     console.log('7 dugar sar 30 or 31');
+//  } else if ( n == 8 ) {
+//     console.log('8 dugar sar 30 or 31');
+//  } else if ( n == 9 ) {
+//     console.log('9 dugar sar 30 or 31');
+//  } else if ( n == 10 ) {
+//     console.log('10 dugar sar 30 or 31');
+//  } else if ( n == 11 ) {
+//     console.log('11 dugar sar 30 or 31');
+//  } else if ( n == 12 ) {
+//     console.log('12 dugar sar 30 or 31');
+//  }
+
+
+   //task7-3 "Өгөгдсөн тоо нь 3-ын үржвэр эсвэл 7-ын үржвэр эсэхийг шалгана уу"
+
+//  let toobishtoo = 12;
+
+//  if ( toobishtoo <= 10 ) {
+//     console.log("urjwer mon");
+//  } else {
+//     console.log("urjwer bish");
+//  }
+
+
+    //task7-4  " n  хувьсагчын утгын дагуу эерэг эсвэл сөрөгийг олж хэвлэнэ."
+
+//  let n1 = -1;
+
+//  if ( n1 >= 0) {
+//     console.log('eyreg');
+//  } else if ( n1 < 0 ) {
+//     console.log('surug');
+//  }
+
+
+    //task7-5     "биеийн жингийн индексийг тооцоx"
+
+//  let weight = 48;
+//  let height = 168;
+
+//  if ( weight/height**2*10000 <= 18.5 ) {
+//     console.log('turanhai');
+//  } else if (weight/height**2*10000 <= 24.9) {
+//     console.log('eruul');
+//  } else if (weight/height**2*10000 <= 29.9) {
+//     console.log('iluudel jintei');
+//  } else if (weight/height**2*10000 <= 34.9) {
+//     console.log('het targalalttai');
+//  }
+
+
+    //task7-6    "Өгөгдсөн тооны тэгш эсвэл сондгой болохыг хэвлэж харуулна уу"
+
+//  let tooo = 3;
+
+//  if (tooo / 2 ==0 ) {
+//     console.log('tesghtoo');
+//  } else {
+//     console.log('sondgoitoo');
+//  }
+
+
+    //task7-7   "nasiig tootsod nasand hargalzah utga hewleh"
+
+//  let yearOfBirth = prompt('ta tursun onoo oruulna uu');
+
+//  if ( 2022-yearOfBirth <= 1 ) {
+//     console.log('infant');
+//  } else if ( 2022-yearOfBirth <= 3 ) {
+//     console.log('toddler');
+//  } else if ( 2022-yearOfBirth <= 5 ) {
+//     console.log('preschool');
+//  } else if ( 2022-yearOfBirth <= 12 ) {
+//     console.log('Gradeschooler');
+//  } else if ( 2022-yearOfBirth <= 18 ) {
+//     console.log('teen');
+//  } else if ( 2022-yearOfBirth <= 21 ) {
+//     console.log('young adult');
+//  } else if ( 2022-yearOfBirth > 21 ) {
+//     console.log('adult');
+//  } 
+
+
+    //task9   "Хоёр багийн дундаж оноог олно уу."
+
+ let aTeam = 96;
+ let aTeam1 = 108;
+ let aTeam2 = 89;
+ let bTeam = 88;
+ let bTeam1 = 91; 
+ let bTeam2 = 110;
+
+ console.log('hoyr bagiin dundaj onoo:', (aTeam+aTeam1+aTeam2)/3+(bTeam+bTeam1+bTeam2)/3);
 
