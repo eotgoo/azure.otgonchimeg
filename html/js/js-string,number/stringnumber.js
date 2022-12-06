d354f11bf5ed974f755566f28777510764f29903
eotgoo <ekotgoo@gmail.com>
2022-12-06 12:13:51 +0800

    gert

--- html/js/js-string,number/stringnumber.js
+++ html/js/js-string,number/stringnumber.js
@@ -1,46 +1,102 @@
-//task1 string
-console.log('Hello I am "Otgoo". \nI am "18" years old. \n This is the new line with tab and some \backslashes\.')
+   //task1 string     "escape sequences. 1 удаагийн хэвлэлтээр үүнийг хэвлэж гаргана уу."
+
+// console.log('Hello I am "Otgoo". \nI am "18" years old. \n This is the new line with tab and some \backslashes\.')
 
 
-//task2-1 string
-let ner = 'otgonchimeg';
-let nernitoo = ner.length;
+   //task2-1 string      "1. Өөрийн нэрийн урт, овог нэрийнхээ уртыг харьцуулж үз."
 
-let ovog = 'erdenechimeg';
-let ovogniitoo = ovog.length;
+// let ner = 'otgonchimeg';
+// let nernitoo = ner.length;
 
-if(nernitoo>=ovogniitoo) {
-    console.log('nernitoo urt');
-} else {
-    console.log('ovogniitoo urt');
-}
+// let ovog = 'erdenechimeg';
+// let ovogniitoo = ovog.length;
 
-
-//task2-2 string
-let text = 'HELLO WORLD';
-console.log(text.toLowerCase());
+// if(nernitoo>=ovogniitoo) {
+//     console.log('nernitoo urt');
+// } else {
+//     console.log('ovogniitoo urt');
+// }
 
 
-//task2-3 string
-let text1 = 'hello world';
-console.log(text1.toUpperCase());
+
+    //task2-2 string     "Бүх тэмдэгтүүдийг жижиг үсгээр бичнэ үү."
+ 
+// let text = 'HELLO WORLD';
+// console.log(text.toLowerCase());
 
 
-//task2-4 string
-let a = 'Pinecone academy';
-let b = a.concat('' ,' ' ,'leap хөтөлбөрт тавтай морилго уу?');
-console.log(b);
+    //task2-3 string     "Бүх тэмдэгтүүдийг том үсгээр бичнэ үү."
+
+// let text1 = 'hello world';
+// console.log(text1.toUpperCase());
 
 
-//task2-5 string
-let firstname = 'Erdene';
-let lastname = 'Otgoo';
-let country = 'Mongolia';
-let city = 'UB';
+    //task2-4 string     "'Pinecone academy- н','leap хөтөлбөрт тавтай морилго уу? нэг мөр болгон нэгтгэнэ үү."
 
-console.log('minii neriig' + ' ' + firstname + ' ' + 'ovogtoi' + ' ' + lastname + ' ' + 'gedeg.' + ' ' + 'bi' + ' ' + country + ' ' + 'ulsiin' + ' ' + city + ' ' + 'hotod amidardag.bye bye bye!!');
+// let a = 'Pinecone academy';
+// let b = a.concat('' ,' ' ,'leap хөтөлбөрт тавтай морилго уу?');
+// console.log(b);
 
 
-//task2-6 string
-let number = 12345;
+    //task2-5 string       "хувьсагч орсон өгүүлбэр зохионо уу. "
 
+// let firstname = 'Erdene';
+// let lastname = 'Otgoo';
+// let country = 'Mongolia';
+// let city = 'UB';
+
+// console.log('minii neriig' + ' ' + firstname + ' ' + 'ovogtoi' + ' ' + lastname + ' ' + 'gedeg.' + ' ' + 'bi' + ' ' + country + ' ' + 'ulsiin' + ' ' + city + ' ' + 'hotod amidardag.bye bye bye!!');
+
+
+
+    //task2-6 string    "Дараах загварыг хэвлэхийн тулд console.log() болон escape тэмдэгтүүдийг ашиглана уу."
+
+// console.log('1 2 3 4 5 \n2 3 4 5 1 \n3 4 5 1 2 \n4 5 1 2 3 \n5 1 2 3 4' );
+
+  
+
+    //task3-1 string      "ognoo hewlen gargah"
+
+// let yy = 2004;
+// let mm = 4;
+// let dd = 6;
+
+// if(yy < 1920 ) {
+//     yy += 1920;
+// }
+// if (mm < 10) {
+//     mm = "0" + mm;
+// }
+// if (dd < 10) {
+//     dd = "0" + dd;
+// }
+// console.log(yy + '-' + mm + '-' +dd);
+
+
+    //task3-2  string      "garaas dugaar oruulan operator shalgah"
+
+// let mobileNumber = prompt('ta dugaara oruulna uu');
+
+// if (mobileNumber.substring(0 ,2 ) >= 90 ) {
+//     console.log('Mobicom');
+// } else if (mobileNumber.substring(0 ,2 ) >= 80) {
+//     console.log('Unitel')
+// } else {
+//     console.log ('buruu dugaar baina')
+// }
+
+
+
+
+       //NUMBER TASKS//
+
+
+    //task1 number     "n butarhai ttoni hamgin oirin buhel too"
+
+// let n = prompt('ta butarhai ta oruulna uu');
+// console.log(Math.round(n));
+
+
+    //task2 number       "3 оронтой той ( n ) ороход тухайн 3 оронтой тооны цифрүүдийн нийлбэрийг ол."
+
+let duriinToo = prompt('ta 3 orontoi too oruulna uu');
