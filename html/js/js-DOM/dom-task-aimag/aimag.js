88b83e945943baadf5f9f73b83886f7cb30a6fa6
Evening <evening@mstars-lab3-10s-iMac.local>
2022-12-14 18:03:13 +0800

    pinecone dr 12/14

---
+++ html/js/js-DOM/dom-task-aimag/aimag.js
@@ -0,0 +1,7 @@
+let first = document.getElementById("aimag-101");
+let second = document.getElementById("aimag-202");
+let last = document.getElementById("aimag-522");
+
+console.log(first);
+console.log(second);
+console.log(last);
---
+++ html/js/js-DOM/toDoListApp/listapp.js
@@ -0,0 +1,37 @@
+let haha = document.getElementById("addList");
+let button = document.getElementById("btn");
+let grouptodo = document.getElementById("grouptodo");
+
+const add = (text) => {
+    let lists = `
+    <div class="listSection">
+        <input type="text" id="addedTask" value="${text}">
+        <i class="fa-solid fa-pen" style="color: black;"></i>
+        <i class="fa-solid fa-check" style="color: green;"></i>
+        <i class="fa-sharp fa-solid fa-trash" style="color: red;"></i>
+    </div>`;
+
+    return lists;
+    // let lists_section = document.createElement("div");
+    // let lol = document.getElementById("addList").value;
+    // lists_section.innerHTML = lists;
+    // grouptodo.appendChild(lists_section);
+    // lists_section.getElementsByTagName("input")[0].value = lol;
+}
+
+const taskAdd = () => {
+    let text = haha.value;
+    if(text === ""){
+        alert("Ta utga oruulaagui baina");
+    }else{
+
+        grouptodo.innerHTML += add(text);
+    }
+}
+
+button.addEventListener('click' , taskAdd);
+document.addEventListener('keyup',(e)=>{
+    if(e.key === "Enter"){
+        taskAdd();
+    }
+});
