a4009d6039f86ae0767ea55ad0ce9d145f39870e
Evening <evening@mstars-lab3-10s-iMac.local>
2022-12-21 17:34:45 +0800

    12/21 pinecone

--- html/js/js-CallbackFunction/calculator/calculator.js
+++ html/js/js-CallbackFunction/calculator/calculator.js
@@ -1,4 +1,18 @@
-function display(val) {
-    document.getElementById('calc').value += val;
-    return val;
+//input
+
+function display(calc) {
+    document.getElementById('calc').value += calc;
+    return calc;
 };
+
+//AC
+
+ const clr= ()=>{
+    calc.value = '';
+ }
+
+//sum
+
+const sum = () => {
+    calc.value += calc.value;
+}
