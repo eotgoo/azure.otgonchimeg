a47fd206a61992eecf49cd59ae6d91e8723788b8
Evening <evening@mstars-lab3-10s-iMac.local>
2022-12-28 19:32:10 +0800

    12/28

--- html/js/js-fetch/main.js
+++ html/js/js-fetch/main.js
@@ -2,10 +2,12 @@
 
 const productList = document.querySelector(".productList");
 const cartList = document.querySelector(".cartList");
-const productsEvent = document.querySelector(".productsEvent");
 const cartCount = document.querySelector(".cartCount");
+const categories = document.querySelector(".categories");
+const cartPrice = document.querySelector(".cartPrice");
 let allProducts = [];
 let cartProducts = [];
+let allCategories = [];
 
 
 const displayProduct = () => {
@@ -21,13 +23,14 @@ const displayProduct = () => {
                   alt="..."
                 />
                 <div class="card-body">
-                  <h5 class="card-title">
+                  <h1 class="card-title">
                     ${product.title}
-                  </h5>
+                  </h1>
+                  <h3>$${product.price}</h3>
                   <p class="card-text text-truncate">
                     ${product.description}
                   </p>
-                  <button onclick="addCart(${idx})"  class="btn btn-primary addCart" >Add card</button>
+                  <button onclick="addCart(${product.id})"  class="btn btn-primary addCart" >Add card</button>
                 </div>
               </div>
             </div>
@@ -54,33 +57,104 @@ const displayCart = () => {
             <div class="offcanvas-body">
               <div class="card mb-3" style="max-width: 540px;">
               <div class="row g-0">
-                <div class="col-md-4">
+                <div class="col-md-3">
                   <img src="${product.thumbnail}" class="img-fluid rounded start" alt="">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-7">
                   <div class="card-body">
                     <h5 class="card-title">${product.title}</h5>
                     <h3>$${product.price}</h3>
                     <div class="counter">
                       <button class="btn btn-light">-</button>
-                      <span class="m-3">0</span>
-                      <button class="btn btn-ligth">+</button>
-                    </div>
-                    <hr>
-                    <div class="mt-2 d-flex justify-content-between">
-                      <p>total price:</p>
-                      <span>$</span>
+                      <span class="m-3">${product.count}</span>
+                      <button class="btn btn-ligth" onClick="sumCount(this)">+</button>
                     </div>
                   </div>
+                </div>
+                <div class="col-md-2">
+                <button type="button" class="btn-close text-reset mt-2 ms-3" data-bs-dismiss="offcanvas" aria-label="Close" onClick="deleteCart(this)"></button>
                 </div>
               </div>
             </div>`;
     cartList.innerHTML += cartItem;
-  }
+  };
+  const totalCartPrice = calculateCartPrice();
+  cartPrice.innerText = `$${totalCartPrice}`;
 };
 
-const addCart = (idx) => {
-  cartProducts.push(allProducts[idx]);
+
+const addCart = (productId) => {
+  const findIdx = cartProducts.findIndex((item) => item.id === productId);
+  if (findIdx > -1) {
+    //ene baraa cartProducts array dotor bval nemehgui harin baraanii too hemjee nemne
+    cartProducts[findIdx].count += 1;
+  } else {
+    //bhgui bol baraag nemne
+    const findIndex = allProducts.findIndex((item) => item.id === productId);
+
+    const newBaraa = { count: 1, ...allProducts[findIndex] };
+    cartProducts.push(newBaraa);
+  }
   cartCount.innerText = cartProducts.length;
   displayCart();
 };
+
+// added cart-delete
+
+const deleteCart = (e) => {
+  const parent = e.parentNode.parentNode.parentNode;
+  const child = e.parentNode.parentNode;
+  parent.removeChild(child);
+  cartProducts.length--;
+  cartCount.innerText=cartProducts.length;
+};
+
+//all categories
+
+const displayCategory = () => {
+  allCategories.forEach((category) => {
+    const categoryItem = `
+    <li><button class="dropdown-item" onclick="getCategoryProduct('${category}')">${category}</button></li>`;
+    categories.innerHTML += categoryItem;
+  })
+};
+
+// all categories dummy json
+
+const getCategory = async () => {
+  const a = await fetch('https://dummyjson.com/products/categories');
+  const data = await a.json();
+  allCategories= data;
+  displayCategory();
+};
+getCategory();
+
+
+// calculator price
+
+const calculateCartPrice = () => {
+  let sumPrice = 0;
+  for (product of cartProducts) {
+    sumPrice = sumPrice + product.price * product.count;
+  }
+  return sumPrice;
+};
+
+// category ylgah dummy json
+
+const getCategoryProduct = async (category) => {
+  console.log(category);
+  const response = await fetch(
+    `https://dummyjson.com/products/category/${category}`
+  );
+  const data = await response.json();
+  allProducts = data.products;
+  displayProduct();
+};
+
+
+//+
+
+
+
+
